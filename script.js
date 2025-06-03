9c88c3ef73ab19d10e0e4e6e2961842a4cda7fce
edbertw <edbertw@connect.hku.hk>
2025-06-03 12:07:42 +0700

    Refactor animation to skill logos

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.skills-section,.skills-grid,.skill-item', {origin:'bottom'})
 ScrollReveal().reveal('.experience-container,.experience-details', {origin:'top'})
 ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
 ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
